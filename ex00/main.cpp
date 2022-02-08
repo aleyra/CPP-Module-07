357eff33e66ff6ba499af8e31a82ac830db7c3bd
lucille <lucille.burnet@gmail.com>
2022-02-08 15:53:24 +0100

    ok

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,21 +1,37 @@
 #include "whatever.hpp"
 
 int	main( void ) {
-	int a = 2;
-	int b = 3;
+	{
+		int a = 2;
+		int b = 3;
 
-	::swap( a, b );
-	std::cout << "a = " << a << ", b = " << b << std::endl;
-	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
-	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+		std::cout << "a = " << a << ", b = " << b << std::endl;//ajouté pour + de clarté
+		::swap( a, b );
+		std::cout << "a = " << a << ", b = " << b << std::endl;
+		std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+		std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
 
-	std::string c = "chaine1";
-	std::string d = "chaine2";
+		std::string c = "chaine1";
+		std::string d = "chaine2";
 
-	::swap(c, d);
-	std::cout << "c = " << c << ", d = " << d << std::endl;
-	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
-	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+		std::cout << "c = " << c << ", d = " << d << std::endl;//ajouté pour + de clarté
+		::swap(c, d);
+		std::cout << "c = " << c << ", d = " << d << std::endl;
+		std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+		std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	}
+
+	//for correction
+
+	{
+		Awesome a(2), b(4);
+
+		std::cout << "a = " << a << ", b = " << b << std::endl;//ajouté pour + de clarté
+		swap(a, b);
+		std::cout << "a = " << a << ", b = " << b << std::endl;//modifié pour + de clarté
+		std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;//modifié pour + de clarté
+		std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;//modifié pour + de clarté
+	}
 
 	return 0;
 }
--- ex00/whatever.hpp
+++ ex00/whatever.hpp
@@ -2,9 +2,11 @@
 # define WHATEVER_H
 
 # include <iostream>
+# include <sstream>
 
 template <typename T>
-void	swap(T a, T b){
+void	swap(T & a, T & b){
+	std::cout << "swap fct called\n";
 	T	tmp = a;
 
 	a = b;
@@ -24,5 +26,25 @@ T const &	max(T const & a, T const &b){
 		return (a);
 	return (b);
 }
+
+//for correction
+class Awesome{
+public:
+	Awesome(void):_n(0){}
+	Awesome(int n):_n(n){}
+	Awesome(Awesome const &src) {this->_n = src._n;}//ajouté car error sinon
+	Awesome & operator=(Awesome & a){_n = a._n; return *this; }
+	bool operator==(Awesome const & rhs) const { return (this->_n == rhs._n); }
+	bool operator!=(Awesome const & rhs) const { return (this->_n != rhs._n); }
+	bool operator>(Awesome const & rhs) const { return(this->_n > rhs._n); }
+	bool operator<(Awesome const & rhs) const { return(this->_n < rhs._n);}
+	bool operator>=(Awesome const & rhs) const { return(this->_n >= rhs._n); }
+	bool operator<=(Awesome const & rhs) const { return(this->_n <= rhs._n);}
+	int get_n() const{return _n;}
+private:
+	int	_n;
+};
+
+std::ostream & operator<<(std::ostream & o, const Awesome &a){ o << a.get_n(); return o;}
 
 #endif
